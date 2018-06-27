b368375b7d16b71881bceaa0c7238fb3c5a3172a
LevertKabe <levertkabi@gmail.com>
2018-06-27 11:32:16 +0200

    Stage 4 Complete-Able to get actual weather API and add the weather to list

--- dev/weather/weather-search.component.ts
+++ dev/weather/weather-search.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "angular2/core";
 import { ControlGroup } from "angular2/common";
+import { WeatherService } from "./weather.service";
+import { WeatherItem } from "./weather-item";
 
 @Component({
     selector: 'weather-search',
@@ -14,13 +16,25 @@ import { ControlGroup } from "angular2/common";
                 <span class="info">City found:</span> City Name
             </div>
         </section>
-    `
+    `,
+    providers: [WeatherService]
 })
 export class WeatherSearchComponent
 {
+
+    constructor(private _weatherService: WeatherService)
+    {
+
+    }
     onSubmit(form: ControlGroup)
     {
-        console.log(form);
+        this._weatherService.searchWeatherData(form.value.location)
+            .subscribe(
+                data => {
+                    const weatherItem = new WeatherItem(data.name, data.weather[0].description, data.main.temp);
+                    this._weatherService.addWeatherItem(weatherItem);
+                }
+            );
     }
 
 }
--- dev/weather/weather.service.ts
+++ dev/weather/weather.service.ts
@@ -1,13 +1,38 @@
 import {Injectable} from 'angular2/core'
 import { WEATHER_ITEMS } from './weather.data';
+import { Http } from 'angular2/http'
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx'
+import { WeatherItem } from './weather-item';
 
 //Injectable  - used when you going to use http,to inject a service or inject other services to this service
 @Injectable()
 export class WeatherService
 {
+    constructor(private _http: Http)
+    {
+
+    }
+
     getWeatherItems()
     {
         return WEATHER_ITEMS;
     }
+
+    addWeatherItem(weatherItem: WeatherItem){
+        WEATHER_ITEMS.push(weatherItem);
+    }
+
+    searchWeatherData(cityName: string): Observable<any>
+    {
+            return this._http.get('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&APPID=dd312d719076159d3d00c7ff908c2a3e&units=metric')
+                                .map(response => response.json())
+                                .catch(error => {
+                                    console.error(error);
+                                    return Observable.throw(error.json())
+                                });
+
+    }
+
 
 }
